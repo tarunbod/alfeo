5f4aca3e2fac13e3e4e6817a038064aeaa099641
Austin Jackson <themacphage@gmail.com>
2014-11-03 22:36:33 -0600

    First completely working prototype

--- js/alfeo.js
+++ js/alfeo.js
@@ -7,5 +7,7 @@ Alfeo.go = function() {
     var cmd = document.getElementById("textarea-main").value;
     var myInterpreter = new Interpreter(cmd);
     myInterpreter.run();
-    alert(myInterpreter.value);
+    //alert(myInterpreter.value);
+    var r = document.getElementById("result-value");
+    r.innerHTML = myInterpreter.value;
 };
