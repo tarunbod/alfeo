d135408321fae265ecaf2d290c2614b92efb8b07
Austin Jackson <themacphage@gmail.com>
2014-11-06 22:45:03 -0600

    Implemented error handling and other functions

--- js/alfeo.js
+++ js/alfeo.js
@@ -1,21 +1,25 @@
 function Alfeo()
 {
-    //
-}
-
-Alfeo.library = new String();
-
-Alfeo.go = function() {
+    var cmd = api + document.getElementById("textarea-main").value;
+    try {
+        var myInterpreter = new Interpreter(cmd);
+        myInterpreter.run();
+        $("#errorbox").addClass("hidden");
+    }
+    catch(err){
+        $("#errorbox").removeClass("hidden");
+        document.getElementById("errorbox").innerHTML = ("<span class=\"errorname\">"+err.name+"</span><br><span class=\"errormessage\">"+err.message+"</span>");
+    }
     
-    var cmd = Alfeo.library + document.getElementById("textarea-main").value;
-    var myInterpreter = new Interpreter(cmd);
-    myInterpreter.run();
     var r = document.getElementById("result-value");
     r.innerHTML = myInterpreter.value;
-};
+}
+
+var api = new String();
 
 
-Alfeo.factorial = function(n) {
+
+function factorial(n) {
     var b = 1;
     while(n > 1)
     {
@@ -23,4 +27,27 @@ Alfeo.factorial = function(n) {
       n--;
     }
     return b;
-};
+}
+
+function pow(a,b) {
+    return Math.pow(a,b);
+}
+function power(a,b) {
+    return Math.pow(a,b);
+}
+function sqrt(a) {
+    return Math.sqrt(a);
+}
+function squareroot(a) {
+    return Math.sqrt(a);
+}
+function nroot(degree,radicand){ // cuberoot of 8 would be nroot(3,8)
+    return Math.pow(Math.abs(radicand), (1.0 / degree));
+}
+//function quadraticFormula(a,b,c){
+//    var s1 = NaN, s2 = NaN;
+//    try{
+//        s1 = ( (-(b)) + Math.sqrt(( b * b) - (4 * a * c)) )  / (2 * a);
+//    }
+//    catch(Exception)
+//}
--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@ function Alfeo()
     //
 }
 
-Alfeo.library = "";
+Alfeo.API = "";
 
 Alfeo.prototype.go = function() {
     
-    var cmd = document.getElementById("textarea-main").value + Alfeo.library;
-    var myInterpreter = new Interpreter(cmd);
-    myInterpreter.run();
+    var cmd = document.getElementById("textarea-main").value + Alfeo.API;
+    
+    try {
+        var myInterpreter = new Interpreter(cmd);
+        myInterpreter.run();
+        $("#errorbox").addClass("invisible");
+    }
+    catch(err){
+        $("#errorbox").removeClass("invisible");
+        document.getElementById("errorbox").innerHTML = ("<span class=\"errorname\">"+err.name+"</span>\n<span class=\"errormessage\">"+err.message+"</span>");
+    }
+    
     var r = document.getElementById("result-value");
     r.innerHTML = myInterpreter.value;
     
